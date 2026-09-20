4524ad02621be9935f9628231d9c8e5e175b5a13
impurify <have@anonymous.sex>
2026-09-20 22:01:45 +0000

    Change template.  (Forces full site rebuild.)
    This is the first significant template change since launch.

--- mk/dirty.mk
+++ mk/dirty.mk
@@ -12,7 +12,8 @@ LOWDOWN_FLAGS=-s \
 	--html-no-skiphtml
 
 DUTR=${HOME}/dutr
-ABSETC=${DUTR}/www/etc
+ABSWWW=${DUTR}/www
+ABSETC=${ABSWWW}/etc
 TEMPLATE=${DUTR}/src/dirty.html
 IMPUREASC=${ABSETC}/dirty-impurify.asc
 
@@ -27,6 +28,9 @@ IMPUREASC=${ABSETC}/dirty-impurify.asc
 		-m "css=`realpath --relative-to=. ${ABSETC}/dutr.css`  `realpath --relative-to=. ${ABSETC}/katex/katex.min.css`" \
 		-m "icon=`realpath --relative-to=. ${ABSETC}/scarlet-prohibition.svg`" \
 		-m "pgpfile=`realpath --relative-to=. ${IMPUREASC}`" \
+		-m "aboutmeurl=`realpath --relative-to=. ${ABSWWW}/about/impurify.html`" \
+		-m "lawyeradurl=`realpath --relative-to=. ${ABSWWW}/personals/first-amendment-lawyers-scholars.html`" \
+		-m "thoughtfuladurl=`realpath --relative-to=. ${ABSWWW}/personals/the-thoughtful-people.html`" \
 		--template ${TEMPLATE} \
 		$< \
 		| \
